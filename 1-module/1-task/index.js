b53e5ce7aa171a610af7a4d08b4a06df8681201c
denis vybornov <vbdenis@yandex.ru>
2021-03-02 23:05:07 +0300

    1/1 factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,17 @@
-function factorial(n) {
-  // ваш код...
+//#1
+// function factorial(n) {
+//   let result = 1;
+//   while (n) {
+//     result *= n--;
+//   }
+//   return result;
+// }
+
+//#2
+function factorial(n){
+  let result = 1;
+  for(let i = 1; i<=n; i++) {
+    result *= i;
+  }
+  return result;
 }
