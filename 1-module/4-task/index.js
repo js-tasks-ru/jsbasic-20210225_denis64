ef34aba934c0347819b21dd625f13c8c5fc98beb
denis.vybornov <denis.vybornov@shtormtech.ru>
2021-03-03 18:58:25 +0300

    spam check

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  let strLower = str.toLowerCase();
+  
+  if((strLower.includes("1xbet")) || (strLower.includes("xxx"))) {
+    return true;   
+  } else {
+    return false
+  }
 }
